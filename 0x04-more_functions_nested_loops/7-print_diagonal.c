8880f555f2e2e033b64cd88ae446cc01f78a9de3
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-17 17:59:22 +0100

    Print diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_line - draws a line.
+ * @n: length parameter
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, j;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			j = i;
+			while (j)
+			{
+				_putchar(' ');
+				j--;	
+			}
+			_putchar('\\');
+			i++;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
