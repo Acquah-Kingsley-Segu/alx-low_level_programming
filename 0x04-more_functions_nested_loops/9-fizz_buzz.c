13aea5904dea09cc9002526e3f3d9a822db9d2d2
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-17 20:57:06 +0100

    Do the fizz-buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - does the fizz buzz test
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
