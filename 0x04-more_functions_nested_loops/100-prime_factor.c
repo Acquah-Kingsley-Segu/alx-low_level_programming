c3cfadbd11247dcc5cc9fc03d091da68ccc30b8a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-18 17:45:12 +0100

    Find the largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int prime_checker(int);
+
+/**
+ * main - prints the largest prime factor of a number
+ * Return: void
+ */
+
+int main(void)
+{
+	long int num; /*Number to find largest prime factor*/
+	
+	int p = 2; /*divisor starting from first prime*/
+
+	int prime_status = 1; /*prime status*/
+
+	int lpf = 2; /*hold the largest prime factor*/
+
+	printf("Enter a number: ");
+	scanf("%li", &num);
+
+	long int num1 = num;
+
+	while (num1 >= (p * p))
+	{
+		if (num1 % p == 0)
+		{
+			prime_status = prime_checker(p);
+
+			if (prime_status)
+			{
+				
+				num1 /= p;
+
+				if (p > lpf)
+				{
+					lpf = p;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+		p++;
+	}
+
+	if (num1 > lpf)
+		printf("Largest prime factor of %li is %li\n", num, num1);
+	else
+		printf("Largest prime factor of %li is %d\n", num, lpf);
+}
+
+
+/**
+ * prime_checker - checks whether a number is a prime 
+ * 
+ * @num: number to be checked 
+ * @return 
+ *	1 if @num is prime
+ *	0 if @num is prime
+ */
+int prime_checker(int num)
+{
+	int i = 2;
+
+	int is_prime = 1;
+
+	while (is_prime && i < num)
+	{
+		if (num % i == 0)
+			is_prime = 0;
+		i++;
+	}
+
+	return is_prime;
+}
