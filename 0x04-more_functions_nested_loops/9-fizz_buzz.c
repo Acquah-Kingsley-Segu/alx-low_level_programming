05a6cfcc89cbafc46ce4c3726133be249a8750c9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-18 22:11:10 +0100

    Perform fixx-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - does the fizz buzz test
- *
- * Return: 0
+ * main - fizzbuzz print ot he console 1-100.
+ * Return: void.
  */
-
 int main(void)
 {
-	int i = 1;
+int i;
 
-	while (i < 101)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d ", i);
+for (i = 1; i < 101; ++i)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
 
-		if (i < 100)
-			printf(" ");
-		i++;
-	}
+if (i < 100)
+printf(" ");
 
-	printf("\n");
+}
+printf("\n");
 
-	return (0);
+return (0);
 }
