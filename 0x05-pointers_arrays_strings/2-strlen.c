433b3ea7ec33f70edfe0bb8abf8eabdb31cfe162
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-22 00:48:24 +0100

    Print lenght of a character string array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+
+/**
+ * _strlen - gets the length of a string
+ * @s: the string character array
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
