9261dda6971a1262354d9edb1c4da1419c8d8931
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-22 23:56:06 +0100

    Copy strig into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies value of one string into another
+ * @dest: value to hold copy
+ * @src: value to be copied
+ * Return: the dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (dest = src);
+}
