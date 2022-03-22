d76a8fecb201bc5a0401ed01fa4ecac0f38bb6bf
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-22 14:18:55 +0100

    Print character array in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - prints characters in reverse
+ * @s: string character array
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	
+	int i = len - 1;
+
+	char *str = &s[len]; 
+
+	while (i >= 0)
+	{
+
+		_putchar(*(--str));
+		i--;
+	}
+
+	_putchar('\n');
+
+}
