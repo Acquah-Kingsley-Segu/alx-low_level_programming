cff6d49de90c751e8ca5ff347c3d111216eaac2a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-22 22:35:06 +0100

    Print characters in even sequence

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints characters in even sequence
+ * @str: pointer to hold character array
+ * Return: void
+ */
+
+
+void puts2(char *str)
+{
+	int len = strlen(str);
+
+	int i = 0;
+
+	while (i < len)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(48 + i);
+		}
+
+		i++;
+	}
+
+	_putchar('\n');
+}
