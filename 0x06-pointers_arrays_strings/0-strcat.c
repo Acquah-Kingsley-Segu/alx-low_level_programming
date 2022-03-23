3d94f9a15fd8e07c9917c994bc323a38a6847d2c
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-23 23:56:43 +0100

    Concatenate one string to the other

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenate a character string to another
+ * @dest: character array to be added to
+ * @src: character array to add
+ * Return: pointer dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	/*int len1 = strlen(dest)*/
+
+	/*int len2 = strlen(src);*/
+
+	char *str;
+
+	while (*str != '\0')
+	{
+		str++;
+	}
+
+	str = src;
+
+	return (dest);
+
+}
