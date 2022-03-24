36b79fc55ddb146e9ad65165281ffc2bd261e12b
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-24 08:24:07 +0100

    Copy one string into another for n-bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a strimg into another for at most n bytes
+ * @dest: destination pointer to be copied into
+ * @src: source pointer
+ * @n: copy byte limit
+ * Return: @dest pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (src[i] == '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
