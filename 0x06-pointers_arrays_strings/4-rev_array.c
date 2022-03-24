c8f874ecdcc2ae25c9e60a760a231371828887e1
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-24 13:23:46 +0100

    Reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,18 +2,28 @@
 
 void reverse_array(int *a, int n)
 {
+	int i;
+	int h;
 
-	int len = n / 2;
-
-	int i = 0, temp;
-
-	while (i < len)
+	for (i = 0; i < (n / 2); i++)
 	{
-		temp = a[n - 1];
-		a[n-1] = a[i];
-		a[i] = temp;
-		
-		n--;
-		i++;
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
 	}
 }
+
+
+/*int len = n / 2;*/
+
+	/*int i = 0, temp;*/
+
+	/*while (i < len)*/
+	/*{*/
+		/*temp = a[n - 1];*/
+		/*a[n-1] = a[i];*/
+		/*a[i] = temp;*/
+		
+		/*n--;*/
+		/*i++;*/
+	/*}*/
