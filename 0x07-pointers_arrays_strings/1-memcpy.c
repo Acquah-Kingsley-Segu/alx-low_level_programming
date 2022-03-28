b32d66092945afb94da34dcdccd20959066c3971
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-28 23:35:56 +0200

    Copy n bytes of data

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - copies n bytes of memory to another location
+ * @dest: destination location
+ * @src: source location
+ * @n: byte to copy
+ * Return: @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
