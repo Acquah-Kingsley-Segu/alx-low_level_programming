21ebdbc7d1c182132ca0773a5b74e83716bbb512
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-03-31 14:18:39 +0200

    Calculate length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculate the length of a string
+ * @s: pointer to string to be used
+ *
+ * Return: lenght of @s
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s != '\0')
+	{
+		s++;
+		len =  1 + _strlen_recursion(s);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return len;
+}
