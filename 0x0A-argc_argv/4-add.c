07c3cc8ac1170b4f765e589631c51e43ec619a0c
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-02 01:10:58 +0200

    Print the sum of integer parameters

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,33 +11,23 @@
  *	1 if not
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
+	int i, sum = 0;
 
-	int sum = 0;
+	if (argc < 1)
+		return (0);
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (!atoi(argv[i]))
 		{
-			if (atoi(argv[i]))
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
-
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
-	else if (argc == 1)
-	{
-		printf("%d\n", sum);
-	}
+	printf("%d\n", sum);
 
 	return (0);
 }
