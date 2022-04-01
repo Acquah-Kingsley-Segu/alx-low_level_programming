37760cf44e8f9dd8b91b9533b736896b55a68249
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-02 01:03:54 +0200

    Print the sum of integer parameters

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two numbers or 'Error'
+ * @argc: number of parameters passed
+ * @argv: parameters passed
+ *
+ * Return:
+ *	0 if successful
+ *	1 if not
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		printf("%d\n", sum);
+	}
+	else if (argc == 1)
+	{
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
