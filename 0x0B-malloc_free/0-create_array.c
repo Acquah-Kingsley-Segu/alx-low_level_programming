d99d63fbaa3639e49fcf29bb02c64ff8e818072f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-05 10:27:10 +0200

    Create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array dynamically
+ * @size: size of array to create
+ * @c: memory size to allocate
+ *
+ * Return:
+ *	pointer to array if successful
+ *	NULL if not sucessful
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	unsigned int i = 0;
+
+	if (size == 0)
+		ptr = NULL;
+	else
+	{
+		ptr = malloc(sizeof(c) * size);
+	}
+
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+
+	return (ptr);
+}
