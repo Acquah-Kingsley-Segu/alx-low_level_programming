2d42c1ab82a2d7d1b101cc10c19ad24de2cba131
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-05 10:59:56 +0200

    Create a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return pointer to a newly allocated space in memory
+ * @str: string to duplicate
+ *
+ * Return:
+ *	pointer to allocated memory if successful
+ *	NULL if it was unsucessful
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	unsigned int len = strlen(str);
+
+	unsigned int i = 0;
+
+	if (str == NULL)
+		ptr = NULL;
+	else
+	{
+		ptr = malloc(sizeof(str[i]) * len);
+
+		if (ptr == NULL)
+			return (NULL);
+
+		while (i < len)
+		{
+			ptr[i] = str[i];
+			i++;
+		}
+	}
+
+	return (ptr);
+}
+
