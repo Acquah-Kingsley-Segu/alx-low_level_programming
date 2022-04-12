e44111e34cd4906d97c151e097d8b0acefbfb68c
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-12 08:24:47 +0200

    Add function prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,14 @@ struct dog
 	char *owner;
 };
 
-/*Function Prototypes*/
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+
+/*Function Prototypes*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
