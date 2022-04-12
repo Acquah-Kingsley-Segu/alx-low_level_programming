b04048972ee22893c5f0ca68e34538b73d8994cf
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-12 09:20:56 +0200

    Create a dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
+	int nc, oc, i, j;
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
@@ -20,22 +22,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+	
+	for (nc = 0; name[nc]; nc++)
+		;
+	nc++;
 
-	new_dog->name = name;
+	new_dog->name = (char *)malloc(sizeof(' ') * nc);
 	if (!new_dog->name)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
+	for (i = 0; i <= nc; i++)
+		new_dog->name[i] = name[i];
+
 	new_dog->age = age;
 
-	new_dog->owner = owner;
+	for (oc = 0; owner[oc]; oc++)
+		;
+	oc++;
+
+	new_dog->owner = (char *)malloc(sizeof(' ') * oc);
 	if (!new_dog->owner)
 	{
 		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
+
+	for (j = 0; j < oc; j++)
+		new_dog->owner[j] = owner[j];
 
 	return (new_dog);
 }
