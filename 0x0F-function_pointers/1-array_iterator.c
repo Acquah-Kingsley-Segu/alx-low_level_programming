56ed61c0c1e395d512f1c3a819d007ebae427fd4
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-14 00:45:21 +0200

    Iterate over over and print element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - loop through an array to be printed
+ * @array: pointer to array to be printed
+ * @size: size of the array
+ * action: function elements will be passed to
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0;
+
+
+	for (; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
