db102942f9c6b47465bbb45713316f4285efe7ed
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-14 00:16:59 +0200

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: pointer to name to be printed
+ * @f: pointer to function to print name
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
