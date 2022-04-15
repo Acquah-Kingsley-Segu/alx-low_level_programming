99e872be9b04af6a45365424489d743e15347400
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-15 23:46:51 +0200

    Print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print strings
+ * @separator: string to separate arguments
+ * @n: numbr of parameters passed
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list opt;
+
+	unsigned int i;
+
+	va_start(opt, n);
+	for(i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(opt, char*));
+		if (separator != 0 && i != n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(opt);
+	printf("\n");
+}
