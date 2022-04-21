58d1daad3e521d1bba637ae776b3121482c6262d
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-21 23:03:45 +0200

    Count list elements and return

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len - counts the number of elements in a list
+ * @h: head pointer of list to print
+ *
+ * Return: the number of elements in h
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	int numOfElem = 0;
+	list_t temp = *h;
+
+	if (!h)
+		return (numOfElem);
+
+	while (temp.next)
+	{
+		numOfElem++;
+		temp = *(temp.next);
+	}
+	numOfElem++;
+
+	return (numOfElem);
+}
