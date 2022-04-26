a9942957302bfa81ecef187b2c852024dc9ad308
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-26 14:02:02 +0200

    Insert a node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,8 +31,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i < idx - 1)
 	{
-		if (temp == NULL)
-			return (NULL);
 		temp = temp->next;
 		i++;
 	}
