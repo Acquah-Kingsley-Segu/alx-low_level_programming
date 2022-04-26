531d46a3cd8555e2b78572e2c5a2fd7385975602
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-26 11:04:11 +0200

    Get node at specified index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - finds node at specified index
+ * @head: head pointer of list
+ * @index: position of node to return
+ *
+ * Return: address of node at indexth position
+ *
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *node = head;
+
+	while (i < index)
+	{
+		node = node->next;
+		i++;
+	}
+
+	return (node);
+}
