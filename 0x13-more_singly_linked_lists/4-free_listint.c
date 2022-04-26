d0d47d1f0c595e3ae01a95f18c0e019b1b834586
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-04-26 09:14:51 +0200

    Free mem space

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_listint - frees memory occupied by list elements
+ * @head: head of list
+ *
+ */
+
 void free_listint(listint_t *head)
 {
-	listint_t *temp = NULL;
+	listint_t *temp;
 
+	if (head == NULL)
+		free(head);
 	while (head)
 	{
-		temp = malloc(sizeof(listint_t));
-		temp = head;
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
