95d52f95c6168f038eef362f23e586512c2b5f42
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-05-27 23:22:33 +0200

    Add two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,13 @@
 
 /**
  * add - adds two integers
- * @num1 - first number
- * @num2 - second number
+ * @num1: first number
+ * @num2: second number
  *
  * Return: num1 + num2
  */
 
 int add(int num1, int num2)
 {
-	return num1 + num2;
+	return (num1 + num2);
 }
