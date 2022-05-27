11ab495b9e6e700955e8e6de25ec2098b8bc34fd
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-05-27 19:26:16 +0200

    A come back

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,12 @@
 
 int print_last_digit(int n)
 {
+	n = n % 10;
 	if (n < 0)
-		n = -n;
-
-	_putchar(48 + (n % 10));
-
-	return (n % 10);
+	{
+		_putchar('0' + (-n));
+		return (-n);
+	}
+	_putchar('0' + n);
+	return ('0' + n);
 }
