d8dfca3a1fa93185c00fe684198a1c167a4556f0
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-05-28 00:00:17 +0200

    Print numbers up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: number to print from
+ *
+ */
+
+void print_to_98(int n)
+{
+	int add;
+
+	if (n == 98)
+		printf("98\n");
+	if (n < 98)
+		add = 1;
+	else
+		add = -1;
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		n += add;
+	}
+	printf("%d\n", n);
+}
