4b316b77a25d67157a535a2ee696fd40ec33e2fc
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-05-28 14:55:22 +0200

    Sum of multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of multiples of 3 and 5 
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
