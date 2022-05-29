ed1e9912843f16e5f3a2d053bbfd91cfc7dcb82f
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-05-29 16:02:58 +0200

    Prints sum of even fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms, followed by a new line
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	long int new, prev = 1, crnt = 2, sum = 2;
+
+	new = prev + crnt;
+	while (new < 4000000)
+	{
+		if (new % 2 == 0)
+			sum += new;
+		prev = crnt;
+		crnt = new;
+		new = prev + crnt;	
+	}
+	printf("%li\n", sum);
+
+	return (0);
+}
