a2982c9fe883da83389e75aea3d2f8f7ee6efa52
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-09 20:52:41 +0200

    free a doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_dlistint - frees memory space held by list elements
+ * @head: pointer to list
+ *
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *node = head;
+
 	if (head == NULL || node == NULL)
 		return;
 	while (head->next != NULL)
