bbf9cacc26f3ec34d55a089a425dabf9e1045a49
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-09 21:16:40 +0200

    return the nth node of a dlistint_t linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * get_dnodeint_at_index - return the nth node from a linked list
+ * @head: head node of list
+ * @index: 0-based position to retrieve
+ *
+ * Return: node at index
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node = NULL;
