b29f07f9c9dbe65e261e66d33edc1e71dae3ae4a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-09 16:54:52 +0200

    Add print list prototype

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -15,5 +16,8 @@ typedef struct dlistint_s
 	int n;
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
-} dlistint_s;
+} dlistint_t;
 
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
